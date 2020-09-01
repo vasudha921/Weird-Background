2af35710d724169c2782e26f4abbe99761f4f86d
vasudha921 <61460922+vasudha921@users.noreply.github.com>
2020-09-01 11:48:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var red = 0;
+var green = 50;
+var blue=255;
+
+
+
+
+function setup(){
+stroke("white");
+  
+  var canvas = createCanvas(1200,400);
+  stroke("white");
+}
+
+
+function draw(){
+  
+  background
+  red.x = [220, 225, 210, 250, 240];
+  green.x = [180, 166, 142, 112, 229];
+  blue.y = [204, 140, 113, 172, 156];
+  
+  stroke("white");
+  
+  ellipse(World.mouseX, World.mouseY, 0);
+  ellipse(220, 225, 210, 250, 240,180, 166, 142, 112, 229,204, 140, 113, 172, 156)
+
+
+  // Use the map() function to do so. 
+
+
+
+  // Remember to fill the canvas with white paint before creating the ellipse.
+}
